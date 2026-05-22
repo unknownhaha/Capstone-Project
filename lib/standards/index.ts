e91345dfff74258fde8c1eb0abcc6a1f95fa62a8
namelessAOE <Bahneendaral@gmail.com>
2026-05-22 15:45:26 +0700

    Run rebuild database add new folder

--- lib/standards/index.ts
+++ lib/standards/index.ts
@@ -4,10 +4,9 @@ import s3 from "./ประตูและหน้าต่าง.json";
 import s4 from "./ที่จอดรถและที่รับส่งผู้โดยสาร.json";
 import s5 from "./ที่พักอาศัยและห้องนอน.json";
 import s6 from "./การครอบครองพิเศษ.json";
-import s7 from "./สิ่งอำนวยความสะดวกอื่นๆ.json";
-import s8 from "./ป้ายสัญลักษณ์.json";
-
+import s7 from "./ป้ายสัญลักษณ์.json";
+import facilities from "./สิ่งอำนวยความสะดวกอื่นๆ"
 
 export const standards = [
-  s1,s2,s3,s4,s5,s6,s7,s8
+  s1,s2,s3,s4,s5,s6,s7, ...facilities
 ];
---
+++ lib/standards/สิ่งอำนวยความสะดวกอื่นๆ/index.ts
@@ -0,0 +1,45 @@
+import s1 from "./การเตือนภัย.json";
+import s2 from "./เครื่องซักและอบผ้า.json";
+import s3 from "./เครื่องเอทีเอ็ม.json";
+import s4 from "./เคานเตอร์คิดเงิน และบริการ.json";
+import s5 from "./ที่เก็บของ.json";
+import s6 from "./ที่ดื่มน้ำ.json";
+import s7 from "./ที่นั่ง.json";
+import s8 from "./ที่นั่ง2.json";
+import s9 from "./ที่นั่งที่โต๊ะ เคานเตอร์ และผิวพื้นที่ทำงาน.json";
+import s10 from "./ที่ปัสสาวะ.json";
+import s11 from "./ที่เปลี่ยนผ้าอ้อม.json";
+import s12 from "./โทรศัพท์.json";
+import s13 from "./พื้นผิวต่างสัมผัสแบบเตือน.json";
+import s14 from "./ระบบช่วยการฟัง.json";
+import s15 from "./ราวจับ.json";
+import s16 from "./ส้วมและห้องส้วม.json";
+import s17 from "./ห้องส้วมและห้องอาบน้ำ.json";
+import s18 from "./ห้องให้นมบุตร.json";
+import s19 from "./ห้องอาบน้ำแบบฝักบัว.json";
+import s20 from "./อ่างล้างหน้า.json";
+import s21 from "./อ่างอาบน้ำ.json";
+
+export default [
+  s1,
+  s2,
+  s3,
+  s4,
+  s5,
+  s6,
+  s7,
+  s8,
+  s9,
+  s10,
+  s11,
+  s12,
+  s13,
+  s14,
+  s15,
+  s16,
+  s17,
+  s18,
+  s19,
+  s20,
+  s21,
+];
